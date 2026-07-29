6716fbf83cd5c6d6999265dae6fb98a7903f639f
niranjan <niranjanram3657@gmail.com>
2026-07-29 15:57:49 +0530

    about

--- app/components/About.js
+++ app/components/About.js
@@ -2,7 +2,8 @@ import { TextGenerateEffect } from "./TextGenerateEffect";
 
 export default function About() {
   const intro =
-    " Welcome, I'm Niranjan Ram - a Software Developer based in Mumbai with a Bachelor's degree in Information Technology, graduated in 2023. I currently work at a product-based company, where I've gained experience developing applications using .NET, MySQL, and AngularJS. Over the past year, I've also worked on modern web development, building responsive and dynamic applications using React, Next.js and MERN stack. I enjoy creating clean, user-centric interfaces and integrating real-world APIs. I'm actively seeking opportunities/collaborations where I can grow as a React developer and contribute to meaningful, design-focused projects.";
+    "Welcome, I'm Niranjan Ram — a Frontend Developer based in Mumbai with a Bachelor's degree in Information Technology. I specialize in building responsive, interactive, and user-focused web applications using React, Next.js, JavaScript, HTML, and CSS. With 2 years of professional experience developing B2B web applications and hands-on experience with modern frontend technologies, I enjoy transforming ideas into fast, accessible, and visually engaging digital experiences. I'm passionate about clean code, thoughtful UI design, animations, and building web experiences that balance performance with usability.";
+
   return (
     <>
       <div className="mx-auto md:w-1/2 px-4 md:px-0 mt-16">
